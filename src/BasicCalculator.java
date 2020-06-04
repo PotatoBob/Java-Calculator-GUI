d22b57d0c2e4c2e0d9d3fe5b7dc72fc8ccd3ddac
Jangwoo Lee <jangwoolee@MacBook-Pro.local>
2020-06-04 16:52:08 -0700

    modified clear and delete behavior

--- src/BasicCalculator.java
+++ src/BasicCalculator.java
@@ -220,7 +220,7 @@ public class BasicCalculator extends JPanel implements ActionListener {
             //replace current JPanel with the Menu Panel
             System.out.println("backToMenu button pressed");
         }
-        if(calcDone) {
+        if(calcDone && e.getSource() != equals && e.getSource() != plus && e.getSource() != minus && e.getSource() != times && e.getSource() != divide) {
             display.setText("");
             calcDone = false;
         }
@@ -361,13 +361,16 @@ public class BasicCalculator extends JPanel implements ActionListener {
         if(e.getSource() == delete) {
 //            display.setText(display.getText().substring(0, display.getText().length()-1));
             int len = display.getText().length();
-            if(len>0) {
-                display.setText(display.getText().substring(0, len-1));
+            if(len>1) {
+                display.setText(display.getText().substring(0, len - 1));
+            } else if(len == 1) {
+                display.setText("0.0");
+                calcDone = true;
             }
         }
         if(e.getSource() == clear) {
-            display.setText("");
-
+            display.setText("0.0");
+            calcDone = true;
         }
     }
 
@@ -380,193 +383,3 @@ public class BasicCalculator extends JPanel implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
-/*
-class ButtonsPanel extends JPanel implements ActionListener {
-    private JButton zero;
-    private JButton one;
-    private JButton two;
-    private JButton three;
-    private JButton four;
-    private JButton five;
-    private JButton six;
-    private JButton seven;
-    private JButton eight;
-    private JButton nine;
-    private JButton point;
-
-    private JButton plus;
-    private JButton minus;
-    private JButton times;
-    private JButton divide;
-
-    private JButton equals;
-    private JButton clear;
-
-    ButtonsPanel() {
-        super(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-
-        zero = new JButton("0");
-        zero.addActionListener(this);
-
-        one = new JButton("1");
-        one.addActionListener(this);
-
-        two = new JButton("2");
-        two.addActionListener(this);
-
-        three = new JButton("3");
-        three.addActionListener(this);
-
-        four = new JButton("4");
-        four.addActionListener(this);
-
-        five = new JButton("5");
-        five.addActionListener(this);
-
-        six = new JButton("6");
-        six.addActionListener(this);
-
-        seven = new JButton("7");
-        seven.addActionListener(this);
-
-        eight = new JButton("8");
-        eight.addActionListener(this);
-
-        nine = new JButton("9");
-        nine.addActionListener(this);
-
-        point = new JButton("\u2219");
-        point.addActionListener(this);
-
-        plus = new JButton("\u002B");
-        plus.addActionListener(this);
-
-        minus = new JButton("\u002D");
-        minus.addActionListener(this);
-
-        times = new JButton("\u00D7");
-        times.addActionListener(this);
-
-        divide = new JButton("\u00F7");
-        divide.addActionListener(this);
-
-        equals = new JButton("\u003D");
-        equals.addActionListener(this);
-
-        clear = new JButton("C");
-        clear.addActionListener(this);
-
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        this.add(clear, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        this.add(divide, gbc);
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        this.add(times, gbc);
-        gbc.gridx = 3;
-        gbc.gridy = 0;
-        this.add(minus, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        this.add(seven, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        this.add(eight, gbc);
-        gbc.gridx = 2;
-        gbc.gridy = 1;
-        this.add(nine, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        this.add(four, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        this.add(five, gbc);
-        gbc.gridx = 2;
-        gbc.gridy = 2;
-        this.add(six, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        this.add(one, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 3;
-        this.add(two, gbc);
-        gbc.gridx = 2;
-        gbc.gridy = 3;
-        this.add(three, gbc);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = 2;
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        this.add(zero, gbc);
-        gbc.gridwidth = 1;
-        gbc.gridx = 2;
-        gbc.gridy = 4;
-        this.add(point, gbc);
-        gbc.fill = GridBagConstraints.VERTICAL;
-        gbc.gridheight = 2;
-        gbc.gridx = 3;
-        gbc.gridy = 1;
-        this.add(plus, gbc);
-        gbc.gridx = 3;
-        gbc.gridy = 3;
-        this.add(equals, gbc);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == zero) {
-
-        }
-        if(e.getSource() == one) {
-
-        }
-        if(e.getSource() == two) {
-
-        }
-        if(e.getSource() == three) {
-
-        }
-        if(e.getSource() == four) {
-
-        }
-        if(e.getSource() == five) {
-
-        }
-        if(e.getSource() == six) {
-
-        }
-        if(e.getSource() == seven) {
-
-        }
-        if(e.getSource() == eight) {
-
-        }
-        if(e.getSource() == nine) {
-
-        }
-        if(e.getSource() == point) {
-
-        }
-        if(e.getSource() == plus) {
-
-        }
-        if(e.getSource() == minus) {
-
-        }
-        if(e.getSource() == times) {
-
-        }
-        if(e.getSource() == divide) {
-
-        }
-        if(e.getSource() == equals) {
-
-        }
-        if(e.getSource() == clear) {
-
-        }
-    }
-}*/
